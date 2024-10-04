345dcfe7ec75ebb274143c682b19bc4bf73569bb
hyphen-o <otekapu@gmail.com>
2024-10-04 11:27:41 +0900

    commitオブジェクトをdumpする機能の追加

--- src/commands/commit.ts
+++ src/commands/commit.ts
@@ -1,5 +1,10 @@
-import { COMMIT_OPTIONS, GIT_INDEX } from "../constants.js";
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+
+import { COMMIT_OPTIONS, GIT_DIR, GIT_HEAD, GIT_INDEX } from "../constants.js";
 import { coloredLog } from "../functions/colored-log.js";
+import { extractHeadHash } from "../functions/extract-head-hash.js";
+import { Commit } from "../models/commit.js";
 import { GitIndex } from "../models/git-index.js";
 import { TreeObject } from "../models/tree-object.js";
 
@@ -29,6 +34,7 @@ export const commit = async (options: Array<string>): Promise<void> => {
     });
   }
 
+  //indexからファイルパスとblobオブジェクトのhashを取得
   const gitIndex = new GitIndex(GIT_INDEX);
   await gitIndex.initialize();
   const fileData = gitIndex.getFileData();
@@ -44,5 +50,35 @@ export const commit = async (options: Array<string>): Promise<void> => {
     return;
   }
 
-  console.log("rootTreeHash: ", rootTreeHash);
+  const parentHash = await extractHeadHash();
+
+  //コミットに含める情報をセットしてdump
+  const commit = new Commit();
+  commit.setCommit({
+    tree: rootTreeHash,
+    parent: parentHash,
+    author: "yamada taro",
+    email: "yamada@gmail.com",
+    message: message,
+  });
+  const commitHash = await commit.dumpCommit();
+
+  //headを最新のコミットhashに更新しておく
+  const headContent = await readFile(GIT_HEAD, "utf8");
+
+  if (headContent.startsWith("ref: ")) {
+    //今回の実装ではmainブランチのみの実装とする
+    const branchPath = "main";
+
+    const branchFilePath = join(GIT_DIR, "refs/heads", branchPath);
+    await writeFile(branchFilePath, commitHash, "utf8");
+
+    console.log(`Updated ${branchPath} branch with commit hash: ${commitHash}`);
+  } else {
+    await writeFile(GIT_HEAD, commitHash, "utf8");
+
+    console.log(
+      `Updated ${parentHash ?? ""} branch with commit hash: ${commitHash}`,
+    );
+  }
 };
